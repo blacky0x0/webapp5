bd6d5902ad724f6e352336a7041cd8376f5c5929
blacky0x0 <blacky0x0@users.noreply.github.com>
2015-01-04 22:57:23 +0300

    Add ArrayStorageTest

--- test/ru/javawebinar/webapp/storage/ArrayStorageTest.java
+++ test/ru/javawebinar/webapp/storage/ArrayStorageTest.java
@@ -8,8 +8,11 @@ import ru.javawebinar.webapp.model.Contact;
 import ru.javawebinar.webapp.model.ContactType;
 import ru.javawebinar.webapp.model.Resume;
 
+import java.util.Arrays;
+
 public class ArrayStorageTest {
     private static Resume R1, R2, R3;
+    private static final int NUMBER_OF_RESUMES = 3;
 
     private ArrayStorage storage = new ArrayStorage();
 
@@ -38,38 +41,71 @@ public class ArrayStorageTest {
 
     @Test
     public void testSave() throws Exception {
+        Resume R4 = new Resume("fullname", "Super location");
+        R4.addContact(new Contact(ContactType.MAIL, "resume@ya.ru"));
 
+        storage.save(R4);
+        Assert.assertEquals(R4, storage.load(R4.getUuid()));
+        Assert.assertEquals(NUMBER_OF_RESUMES + 1, storage.size());
+    }
+
+    @Test (expected = Exception.class)
+    public void testSaveNull() throws Exception {
+        storage.save(null);
+        Assert.assertEquals(NUMBER_OF_RESUMES, storage.size());
+    }
+
+    @Test (expected = Exception.class)
+    public void testSaveClone() throws Exception {
+        storage.save(R1);
+        Assert.assertEquals(NUMBER_OF_RESUMES, storage.size());
     }
 
     @Test
     public void testClear() throws Exception {
-
+        storage.clear();
+        Assert.assertEquals(0, storage.size());
     }
 
     @Test
     public void testUpdate() throws Exception {
+        String updatedName = "Updated name";
+        String updatedLocation = "Updated location";
 
+        R3.setFullName(updatedName);
+        R3.setLocation(updatedLocation);
+
+        storage.update(R3);
+        Assert.assertEquals(R3, storage.load(R3.getUuid()));
+        Assert.assertEquals(R3.getFullName(), storage.load(R3.getUuid()).getFullName());
+        Assert.assertEquals(R3.getLocation(), storage.load(R3.getUuid()).getLocation());
+        Assert.assertEquals(NUMBER_OF_RESUMES, storage.size());
     }
 
     @Test
     public void testLoad() throws Exception {
+        Assert.assertEquals(R3, storage.load(R3.getUuid()));
+    }
 
+    @Test (expected = Exception.class)
+    public void testLoadEmptyUuid() throws Exception {
+        Assert.assertEquals(R3, storage.load(""));
     }
 
     @Test
     public void testDelete() throws Exception {
-        storage.delete(R1.getUuid());
-        Assert.assertEquals(2, storage.size());
-        Assert.assertEquals(null, storage.load(R1.getUuid()));
+        storage.delete(R3.getUuid());
+        Assert.assertEquals(null, storage.load(R3.getUuid()));
+        Assert.assertEquals(NUMBER_OF_RESUMES - 1, storage.size());
     }
 
     @Test
     public void testGetAllSorted() throws Exception {
-
+        Assert.assertEquals(Arrays.asList(R1, R2, R3).toString(), storage.getAllSorted().toString());
     }
 
     @Test
     public void testSize() throws Exception {
-        Assert.assertEquals(3, storage.size());
+        Assert.assertEquals(NUMBER_OF_RESUMES, storage.size());
     }
 }
