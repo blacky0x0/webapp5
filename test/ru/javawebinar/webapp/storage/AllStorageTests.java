3729bcb0bb01a5020995642f0059eb21b2c55d2c
blacky0x0 <blacky0x0@users.noreply.github.com>
2015-02-08 10:43:11 +0300

    Add JsonFileStorageTest & XmlFileStorageTest to suit of tests in AllStorageTests

--- test/ru/javawebinar/webapp/storage/AllStorageTests.java
+++ test/ru/javawebinar/webapp/storage/AllStorageTests.java
@@ -8,6 +8,13 @@ import org.junit.runners.Suite;
  * 09.01.2015.
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ArrayStorageTest.class, MapStorageTest.class, DataStreamFileStorageTest.class, SerializeFileStorageTest.class})
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
+        MapStorageTest.class,
+        DataStreamFileStorageTest.class,
+        SerializeFileStorageTest.class,
+        JsonFileStorageTest.class,
+        XmlFileStorageTest.class
+})
 public class AllStorageTests {
 }
